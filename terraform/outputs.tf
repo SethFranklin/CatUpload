2eac049ac90b64fde17ad00120b0f0f8370113f3
Seth Franklin <sethfranklin@Seths-MacBook-Air.local>
2025-06-23 17:56:32 -0400

    Moved folder structure

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,9 +7,11 @@ output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.website.domain_name
 }
 
+/*
 output "ec2_instance_public_ip_address" {
   value = aws_eip.server.address
 }
+*/
 
 output "s3_bucket_name" {
   value = aws_s3_bucket.cat.bucket
